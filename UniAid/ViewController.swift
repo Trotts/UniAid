15c1fa550a03ec226aa1f15cc8c033be304289dd
CameronTrotter <c.trotter2@newcastle.ac.uk>
2016-03-12 16:46:29 -0400

    Worked on Recorder, have basic Buttons (Record and Play); can record and play audio.

--- UniAid/ViewController.swift
+++ UniAid/ViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDelegate{
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,8 +20,98 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
   }
-  //lets go Cameron
-    //now its my turn -- amr
+  // BEGIN RECORDER FUNCTIONS - Cameron
+    
+    @IBOutlet weak var RecordButton: UIButton!
+    @IBOutlet weak var PlayButton: UIButton!
+    
+    var soundRecorder :  AVAudioRecorder!
+    var soundPlayer : AVAudioPlayer!
+    var fileName = "audioFile.m4a"
+    
+    // Gets Directory for storing the audio file
+    func getCacheDirectory() -> String{
+        let paths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+        return paths[0]
+    }
+    
+    // Gets the audio file URL from the Directory
+    func getFileURL() -> NSURL{
+        let path = NSURL(fileURLWithPath: NSTemporaryDirectory()).URLByAppendingPathComponent(fileName)
+        return path
+    }
+    
+    // Settings for the recorder: Format, Encoding, Quality, Bit Rate; sets to record
+    func setRecorder(){
+        let recordSettings : [String : AnyObject] = [AVFormatIDKey : NSNumber(unsignedInt : kAudioFormatAppleLossless), AVEncoderAudioQualityKey : AVAudioQuality.Max.rawValue as NSNumber, AVEncoderBitRateKey : 320000 as NSNumber, AVNumberOfChannelsKey : 2 as NSNumber, AVSampleRateKey : 44100.0 as NSNumber ]
+        do{
+            soundRecorder = try AVAudioRecorder(URL: getFileURL(), settings: recordSettings)
+            soundRecorder.delegate = self
+            soundRecorder.prepareToRecord()
+        }
+        catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
 
+    // Record Button
+    @IBAction func Record(sender: UIButton){
+        // When Record button is hit, start recording, change to Stop, disable Play Button
+        if sender.titleLabel?.text == "Record"{
+            soundRecorder.record()
+            sender.setTitle("Stop", forState: .Normal)
+            PlayButton.enabled = false
+        }
+        // When button is "Stop" and hit, stop recording, set to record, enable play
+        else{
+            soundRecorder.stop()
+            sender.setTitle("Record", forState: .Normal)
+            PlayButton.enabled = false
+        }
+    }
+    
+    //Play Button
+    @IBAction func PlayRecord(sender: UIButton){
+        // When Play button is hit, start playing file, change to Stop, disable Record Button
+        if sender.titleLabel?.text == "Play"{
+            RecordButton.enabled = false
+            sender.setTitle("Stop", forState: .Normal)
+            preparePlayer()
+            soundPlayer.play()
+        }
+        // When button is "Stop" and hit, stop playing, set to Play
+        else{
+            soundPlayer.stop()
+            sender.setTitle("Play", forState: .Normal)
+        }
+    }
+    
+    // Sets up the Audio Player
+    func preparePlayer(){
+        do{
+            soundPlayer =  try AVAudioPlayer(contentsOfURL: getFileURL())
+            soundPlayer.delegate = self
+            soundPlayer.prepareToPlay()
+            soundPlayer.volume = 1.0
+            
+        }
+        catch let error as NSError{
+            print(error.localizedDescription)
+
+        }
+    }
+    // When Recording is finished, enable Play Button
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
+        PlayButton.enabled = true
+    }
+    // When Audio Player is finished, enable Record Button
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer, successfully flag: Bool) {
+        RecordButton.enabled = true
+        PlayButton.setTitle("Play", forState: .Normal)
+    }
+    
+    //END RECORDER FUNCTIONS - Cameron
+    
 }
+
 
