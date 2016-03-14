9bf6847e0f8ff7d9f4bfca936be6700c8be9d4b1
igor epshtein <epshtein.igor@gmail.com>
2016-03-14 14:04:35 -0300

    Added side-slide menu to the main view controller

---
+++ UniAid/BackTableVC.swift
@@ -0,0 +1,34 @@
+//
+//  BackTableVC.swift
+//  UniAid
+//
+//  Created by igor epshtein on 2016-03-14.
+//  Copyright © 2016 igor epshtein. All rights reserved.
+//
+
+import Foundation
+
+class BackTableVC: UITableViewController {
+    var TableArray = [String]()
+    
+    override func viewDidLoad() {
+        TableArray = ["Home","Notes","Record"]
+    }
+    
+    
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TableArray.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
+            cell.textLabel?.text = TableArray[indexPath.row]
+            
+        
+        
+            return cell
+        }
+    
+}
--- UniAid/ViewController.swift
+++ UniAid/ViewController.swift
@@ -10,17 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var open: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        open.target = self.revealViewController()
+        open.action = Selector("revealToggle:")
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
   }
-  //lets go Cameron
-    //now its my turn -- amr
 
 }
 
