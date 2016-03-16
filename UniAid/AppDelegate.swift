35fe2af54f6da5020618b8c058537e7865d858ae
igor epshtein <epshtein.igor@gmail.com>
2016-03-16 03:25:10 -0300

    update to main screen

--- UniAid/AppDelegate.swift
+++ UniAid/AppDelegate.swift
@@ -15,9 +15,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
         // Override point for customization after application launch.
+        UINavigationBar.appearance().tintColor=UIColor.orangeColor()
+        
+//        let naviBgImage:UIImage = UIImage(named: "naviBgImage.png")!
+//        UINavigationBar.appearance().setBackgroundImage(naviBgImage, forBarMetrics: .Default)
+        
+        
         return true
     }
+    
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
