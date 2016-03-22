d798a2eb5a015bd1ff73c1642c611059a5730401
igor epshtein <epshtein.igor@gmail.com>
2016-03-22 18:58:32 -0300

    added tableview to the main view

---
+++ UniAid/CoursesCellsTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CoursesCellsTableViewCell.swift
+//  UniAid
+//
+//  Created by igor epshtein on 2016-03-22.
+//  Copyright © 2016 igor epshtein. All rights reserved.
+//
+
+import UIKit
+
+class CoursesCellsTableViewCell: UITableViewCell {
+
+    @IBOutlet var courseLable: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- UniAid/ViewController.swift
+++ UniAid/ViewController.swift
@@ -1,9 +1,11 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate ,UITableViewDataSource {
     
     @IBOutlet var open: UIBarButtonItem!
-
+    @IBOutlet var tableView: UITableView!
+    
+    var studentCorses = [Course(course: "CSCI", courseNum: 4414, prof: "Nauzer", profEmail: "profEmail: aaa@gmail.com", buildingName: "Howe", scheduale: ["s","w","f"])]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,5 +19,22 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return studentCorses.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! CoursesCellsTableViewCell
+        var courseName = studentCorses[indexPath.row].Course
+        let courseNum = String(studentCorses[indexPath.row].CourseNumber)
+        print(courseNum)
+        courseName += " "
+        
+        cell.courseLable.text = courseName
+        
+        return cell
+        
     }
 }
