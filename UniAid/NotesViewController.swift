9bc6bf3b7f3967ac7e5366725d886e207b02a012
CameronTrotter <c.trotter2@newcastle.ac.uk>
2016-03-26 20:17:59 -0300

    Merging the Recorder and the Notes Views

--- UniAid/NotesViewController.swift
+++ UniAid/NotesViewController.swift
@@ -1,4 +1,3 @@
-//
 //  NotesViewController.swift
 //  NotesTest
 //
@@ -7,19 +6,29 @@
 //
 
 import UIKit
+import AVFoundation
 
 protocol NoteViewDelegate {
     func didUpdateNoteWithTitle(newTitle : String, andBody newBody :
         String)
 }
 
-class NotesViewController: UIViewController, UITextViewDelegate {
+class NotesViewController: UIViewController, UITextViewDelegate, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
     
+    //For the Notes
     var delegate : NoteViewDelegate?
     @IBOutlet weak var txtBody : UITextView!
     @IBOutlet weak var btnDoneEditing: UIBarButtonItem!
     var strBodyText : String!
     
+    //For the Recorder
+    @IBOutlet weak var RecordButton: UIButton!
+    @IBOutlet weak var PlayButton: UIButton!
+    var soundRecorder :  AVAudioRecorder!
+    var soundPlayer : AVAudioPlayer!
+    var fileName = "audioFile.m4a"
+    
+    //MARK: General Functions
     override func viewDidLoad() {
         super.viewDidLoad()
         //Set text to String
@@ -43,12 +52,15 @@ class NotesViewController: UIViewController, UITextViewDelegate {
         }
     }
     
+    //MARK: Notes Functions
+    
+    //Sets the color of the Hide Keyboard button when keyboard is up
     func textViewDidBeginEditing(textView: UITextView) {
-        //Sets the color of the Hide Keyboard button when keyboard is up
         self.btnDoneEditing.tintColor = UIColor(red: 255.0/255.0, green:
             205.0/255.0, blue: 096.0/255.0, alpha: 1) 
     }
     
+    //Set title for note
     func textViewDidChange(textView: UITextView) {
         //Separate the body into multiple sections
         let components = self.txtBody.text.componentsSeparatedByString("\n")
@@ -74,4 +86,91 @@ class NotesViewController: UIViewController, UITextViewDelegate {
             self.delegate!.didUpdateNoteWithTitle( self.navigationItem.title!, andBody: self.txtBody.text)
         }
     }
+    
+    //MARK: Recorder Functions
+    
+    // Gets Directory for storing the audio file
+    func getCacheDirectory() -> String{
+        let paths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+        return paths[0]
+    }
+    
+    // Gets the audio file URL from the Directory
+    func getFileURL() -> NSURL{
+        let path = NSURL(fileURLWithPath: NSTemporaryDirectory()).URLByAppendingPathComponent(fileName)
+        return path
+    }
+    
+    // Settings for the recorder: Format, Encoding, Quality, Bit Rate; sets to record
+    func setRecorder(){
+        let recordSettings : [String : AnyObject] = [AVFormatIDKey : NSNumber(unsignedInt : kAudioFormatAppleLossless), AVEncoderAudioQualityKey : AVAudioQuality.Max.rawValue as NSNumber, AVEncoderBitRateKey : 320000 as NSNumber, AVNumberOfChannelsKey : 2 as NSNumber, AVSampleRateKey : 44100.0 as NSNumber ]
+        do{
+            soundRecorder = try AVAudioRecorder(URL: getFileURL(), settings: recordSettings)
+            soundRecorder.delegate = self
+            soundRecorder.prepareToRecord()
+        }
+        catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+    
+    // Record Button
+    @IBAction func Record(sender: UIButton){
+        // When Record button is hit, start recording, change to Stop, disable Play Button
+        if sender.titleLabel?.text == "Record"{
+            setRecorder()
+            soundRecorder.record()
+            sender.setTitle("Stop", forState: .Normal)
+            PlayButton.enabled = false
+        }
+            // When button is "Stop" and hit, stop recording, set to record, enable play
+        else{
+            soundRecorder.stop()
+            sender.setTitle("Record", forState: .Normal)
+            PlayButton.enabled = false
+        }
+    }
+    
+    //Play Button
+    @IBAction func PlayRecord(sender: UIButton){
+        // When Play button is hit, start playing file, change to Stop, disable Record Button
+        if sender.titleLabel?.text == "Play"{
+            RecordButton.enabled = false
+            sender.setTitle("Stop", forState: .Normal)
+            preparePlayer()
+            soundPlayer.play()
+        }
+            // When button is "Stop" and hit, stop playing, set to Play
+        else{
+            soundPlayer.stop()
+            sender.setTitle("Play", forState: .Normal)
+            RecordButton.enabled = true
+        }
+    }
+    
+    // Sets up the Audio Player
+    func preparePlayer(){
+        do{
+            soundPlayer =  try AVAudioPlayer(contentsOfURL: getFileURL())
+            soundPlayer.delegate = self
+            soundPlayer.prepareToPlay()
+            soundPlayer.volume = 1.0
+            
+        }
+        catch let error as NSError{
+            print(error.localizedDescription)
+            
+        }
+    }
+    // When Recording is finished, enable Play Button
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
+        PlayButton.enabled = true
+    }
+    // When Audio Player is finished, enable Record Button
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer, successfully flag: Bool) {
+        RecordButton.enabled = true
+        PlayButton.setTitle("Play", forState: .Normal)
+    }
+
+    
 }
